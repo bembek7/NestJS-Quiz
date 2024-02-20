37c49c3240194a7da3d46e9b2e870a47eed28298
bpucilowski <pucilowskibartlomiej@gmail.com>
2024-02-20 10:49:36 +0100

    Validate input for createQuiz function

--- src/services/QuizService.ts
+++ src/services/QuizService.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Quiz } from 'src/models/Quiz';
-import { Question } from 'src/models/Question';
+import { Question, QuestionType } from 'src/models/Question';
 import { CreateQuizInput } from 'src/input types/CreateQuizType';
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 
@@ -29,6 +29,65 @@ export class QuizService {
 
   async createQuiz(createQuizData: CreateQuizInput) {
     const { quiz, questions } = createQuizData;
+
+    // input validation
+    let questionNr = 0;
+    for (const question of questions) {
+      questionNr++;
+      switch (question.questionType) {
+        case QuestionType.SingleAnswer: {
+          if (question.rightAnswers.length != 1) {
+            throw new BadRequestException(
+              `Answer for a single correct answer type of question must have the size of 1. Question nr ${questionNr}`,
+            );
+          }
+          if (question.answers.length < 2) {
+            throw new BadRequestException(
+              `You have to provide at least 2 possible answers for single correct answer type of question. Question nr ${questionNr}`,
+            );
+          }
+          break;
+        }
+        case QuestionType.MultipleAnswer: {
+          if (
+            question.rightAnswers.length > question.answers.length ||
+            question.rightAnswers.length < 1
+          ) {
+            throw new BadRequestException(
+              `Array of answers for a multiple correct answer type of question must have the size of 1-(number of possible answers). Question nr ${questionNr} `,
+            );
+          }
+          if (question.answers.length < 2) {
+            throw new BadRequestException(
+              `You have to provide at least 2 possible answers for multiple correct answers type of question. Question nr ${questionNr}`,
+            );
+          }
+          break;
+        }
+        case QuestionType.Sort: {
+          if (question.rightAnswers.length !== question.answers.length) {
+            throw new BadRequestException(
+              `Array of answers for a sort answers type of question should have the size of (number of possible answers). Question nr ${questionNr}`,
+            );
+          }
+          if (question.answers.length < 2) {
+            throw new BadRequestException(
+              `You have to provide at least 2 possible answers for answers sorting type of question. Question nr ${questionNr}`,
+            );
+          }
+          break;
+        }
+        case QuestionType.TextAnswer: {
+          if (question.answers && question.answers.length > 0) {
+            throw new BadRequestException(
+              `You cannot provide possible answers for text answer type of question. Question nr ${questionNr}`,
+            );
+          }
+          break;
+        }
+      }
+    }
+
     const newQuiz = this.quizzesRepository.create(quiz);
     const savedQuiz = await this.quizzesRepository.save(newQuiz);
 
