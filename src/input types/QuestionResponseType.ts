88599e7018db813a2bc10a07c9863d7fb1c77e7e
bpucilowski <pucilowskibartlomiej@gmail.com>
2024-02-20 10:48:47 +0100

    Add question response type for providing the answers

---
+++ src/input types/QuestionResponseType.ts
@@ -0,0 +1,10 @@
+import { Field, ID, InputType } from '@nestjs/graphql';
+
+@InputType()
+export class QuestionResponse {
+  @Field(type => ID)
+  questionId: string;
+
+  @Field(type => [String])
+  answers: [string];
+}
